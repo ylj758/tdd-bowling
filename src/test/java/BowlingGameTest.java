94ddb192d367f0bdd62e88cf5e64ff9329743434
ylj <758766951@qq.com>
2020-09-19 20:58:51 +0800

    [ylj]: implement  PPrevious nine rounds:strike and spare，tenth round: no strike or spare

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -56,23 +56,21 @@ public class BowlingGameTest {
         assertEquals(246, bowlingGame.calculateBowlingGameScore(pinArray));
     }
 
-
-
-//>* 7.Given
-//>* 8.Given Previous nine rounds:strike and spare，tenth round: no strike or spare
 //>* 9.Given Previous nine rounds:strike and spare and no strike or spare，tenth round: no strike or spare
 //    @Test
 //    public void shold_previous_nine_rounds_strike_and_spare_and_no_strike_or_spare_tenth_round_strike(){
 //        BowlingGame bowlingGame = new BowlingGame();
 //        int[] pinArray = {1,3,10,1,10,3,10,10,10,10,10,10,10,10,10};
+
+//        System.out.println(bowlingGame.calculateBowlingGameScore(pinArray));
 //        assertEquals(4+21+13+30+30+30+30+30+30+10+10+10, bowlingGame.calculateBowlingGameScore(pinArray));
 //    }
 //
-//    @Test
-//    public void shold_previous_nine_rounds_strike_and_spare_and_no_strike_or_spare_tenth_round_strike(){
-//        BowlingGame bowlingGame = new BowlingGame();
-//        int[] pinArray = {1,3,10,1,10,3,10,10,10,10,10,10,10,10,10};
-//        assertEquals(4+21+13+30+30+30+30+30+30+10+10+10, bowlingGame.calculateBowlingGameScore(pinArray));
-//    }
+    @Test
+    public void shold_previous_nine_rounds_strike_and_spare_tenth_round_no_strike_or_spare(){
+        BowlingGame bowlingGame = new BowlingGame();
+        int[] pinArray = {1,3,10,1,10,3,10,10,10,10,10,10,1,2};
+        assertEquals(194, bowlingGame.calculateBowlingGameScore(pinArray));
+    }
 
 }
